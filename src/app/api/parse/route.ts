b23552ebe69d4c578b1d5e2a611f74d3ef71464b
Sam Bautista <tadeolinco@gmail.com>
2025-03-13 13:53:34 +0800

    Remove edge runtime

--- src/app/api/parse/route.ts
+++ src/app/api/parse/route.ts
@@ -1,7 +1,5 @@
 import { generateNEM12Blocks } from "@/lib/parse-generator";
 
-export const runtime = "edge";
-
 export async function POST(request: Request) {
   const formData = await request.formData();
   const file = formData.get("file") as File;
