9cf710fd56427269fe908c858a048ab1a54b923f
Sam Bautista <tadeolinco@gmail.com>
2025-03-13 14:00:57 +0800

    Override size limit

--- src/app/api/parse/route.ts
+++ src/app/api/parse/route.ts
@@ -1,5 +1,13 @@
 import { generateNEM12Blocks } from "@/lib/parse-generator";
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "250mb",
+    },
+  },
+};
+
 export async function POST(request: Request) {
   const formData = await request.formData();
   const file = formData.get("file") as File;
