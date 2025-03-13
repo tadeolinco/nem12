90861927e62558413964dc67237a954939551e5e
Sam Bautista <tadeolinco@gmail.com>
2025-03-13 10:38:48 +0800

    Add options to for page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
 import { parseNEM12 } from "@/lib/parse";
+import { parseNEM12 as parseNEM12Sync } from "@/lib/parse-sync";
 
 export default async function Home() {
   async function onSubmit(formData: FormData) {
     "use server";
 
     try {
+      const type = formData.get("type") || "async";
       const file = formData.get("file") as File;
       const text = await file.text();
 
       const now = performance.now();
-      const dataBlock = await parseNEM12(text);
+
+      if (type === "async") {
+        await parseNEM12(text);
+      } else {
+        parseNEM12Sync(text);
+      }
+
       const end = performance.now();
 
-      console.log(`TOTAL Time taken: ${end - now} milliseconds`);
-      console.log(dataBlock);
+      console.log(`TOTAL ${type} Time taken: ${end - now} milliseconds`);
     } catch (error) {
       console.log(error);
     }
@@ -22,6 +29,14 @@ export default async function Home() {
   return (
     <form action={onSubmit} method="POST">
       <input type="file" name="file" />
+      <label>
+        <input type="radio" name="type" value="async" />
+        Async
+      </label>
+      <label>
+        <input type="radio" name="type" value="sync" />
+        Sync
+      </label>
       <button type="submit">Parse</button>
     </form>
   );
